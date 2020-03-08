f0e21c4fd7a0431c3087137a1bd306c239ff5ed6
Jeremias ramirez <jereramirez499@gmail.com>
2020-03-08 17:48:48 -0400

    maquetacion y funcionalidad de transferir items creada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
- 
+
   public europe :any[]= ["France", "Italy", "Germany", "Suiza","Uruguay", "Polonia", "Austria","El salvador"]
   public latin :any[]= ["Colombia", "Reino unido", "Argentina",  "Grecia", "Paraguay", "Honduras"]
 
+  drop(event: CdkDragDrop<string[]>) {
+      if (event.previousContainer === event.container) {
+        moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      } else {
+        transferArrayItem(event.previousContainer.data,
+                          event.container.data,
+                          event.previousIndex,
+                          event.currentIndex);
+      }
+    }
 
 }
